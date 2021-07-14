4b9114db29d53c2b2776fa0cac32471839c3809f
Ben Delaney <ben@bdelaney.me>
2021-07-15 02:16:18 +1000

    fix: prepare script breaks if ns --version output not perfect

--- src/scripts/prepare.js
+++ src/scripts/prepare.js
@@ -1,18 +1,25 @@
-const { exec } = require('child_process');
+const { execSync } = require('child_process');
+const { existsSync } = require('fs');
+const { basename, join } = require('path');
 const semver = require('semver');
 
-exec('tns --version', (err, stdout, stderr) => {
-    if (err) {
-        // node couldn't execute the command
-        console.log(`tns --version err: ${err}`);
-        return;
-    }
+const pluginDir = join(__dirname, '..')
+const aarFile = join(pluginDir, 'platforms', 'android', 'nativescript_google_maps_sdk.aar')
 
-    const tnsVersion = semver.major(stdout);
-
-    // execute 'tns plugin build' for {N} version > 4. This command builds .aar in platforms/android folder.
-    if (tnsVersion >= 4) {
-        console.log(`executing 'tns plugin build'`);
-        exec('tns plugin build');
+if (existsSync(aarFile)) {
+    console.log(`nativescript-google-maps prepare.js: ✅ ${basename(aarFile)} already exists, skipping ns plugin build`)
+} else {
+    let command = `ns --version`
+    try {
+        const nsVer = execSync(command).toString().split(/\r?\n/g).find(l => /^\d[0-9.]+\d$/.test(l))
+        const nsVersion = semver.major(nsVer);
+        
+        // execute 'ns plugin build' for {N} version > 4. This command builds .aar in platforms/android folder.
+        if (nsVersion >= 4) {
+            command = `ns plugin build`
+            execSync(command, { stdio: 'inherit', cwd: pluginDir })
+        }
+    } catch (error) {
+        console.log(`prepare.js error. Last command: ${command}`, error)
     }
-}); 
+}
